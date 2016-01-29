480b8aa70035c05b80a307d2142144d2469db178
U2ware <u2waremanager@gmail.com>
2016-01-29 16:14:25 +0900

    SNMP Support

--- spring-integration-u2ware-snmp/src/test/java/io/github/u2ware/integration/snmp/outbound/SnmpOutboundChannelAdapterTests.java
+++ spring-integration-u2ware-snmp/src/test/java/io/github/u2ware/integration/snmp/outbound/SnmpOutboundChannelAdapterTests.java
@@ -1,52 +1,45 @@
-package io.github.u2ware.integration.bacnet.outbound;
+package io.github.u2ware.integration.snmp.outbound;
 
-import io.github.u2ware.integration.snmp.core.BacnetRequest;
-import io.github.u2ware.integration.snmp.core.BacnetSlave;
+import io.github.u2ware.integration.snmp.core.SnmpAgent;
+import io.github.u2ware.integration.snmp.core.SnmpRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.PollableChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
-public class BacnetOutboundGatewayTests {
+public class SnmpOutboundChannelAdapterTests {
 
 	@BeforeClass
 	public static void beforeClass() throws Exception {
-		BacnetSlave.startup(37803);
+		SnmpAgent.startup(10171);
 	}
 
 	@AfterClass
 	public static void afterClass() throws Exception{
-		BacnetSlave.shutdown();
+		SnmpAgent.shutdown(10171);
 	}
 
 	protected Log logger = LogFactory.getLog(getClass());
-
-	@Autowired @Qualifier("bacnetRequest")
-	private MessageChannel bacnetRequest;
-
-	@Autowired @Qualifier("bacnetResponse")
-	private PollableChannel bacnetResponse;
+	
+	@Autowired @Qualifier("snmpRequest")
+	private MessageChannel snmpRequest;
 
     @Test
 	public void testRunning() throws Exception {
 
-    	BacnetRequest payload = new BacnetRequest("127.0.0.1:37803", 37803);
-		bacnetRequest.send(MessageBuilder.withPayload(payload).build());
-
-		Object receive = bacnetResponse.receive(10000);
-		Assert.assertNotNull(receive);
+    	SnmpRequest payload = new SnmpRequest("127.0.0.1", 10171, "1.3.6");
+    	snmpRequest.send(MessageBuilder.withPayload(payload).build());
+		Thread.sleep(3000);
 	}
 }
--- spring-integration-u2ware-snmp/src/test/java/io/github/u2ware/integration/snmp/outbound/SnmpOutboundGatewayTests.java
+++ spring-integration-u2ware-snmp/src/test/java/io/github/u2ware/integration/snmp/outbound/SnmpOutboundGatewayTests.java
@@ -1,45 +1,53 @@
-package io.github.u2ware.integration.bacnet.outbound;
+package io.github.u2ware.integration.snmp.outbound;
 
-import io.github.u2ware.integration.snmp.core.BacnetRequest;
-import io.github.u2ware.integration.snmp.core.BacnetSlave;
+import io.github.u2ware.integration.snmp.core.SnmpAgent;
+import io.github.u2ware.integration.snmp.core.SnmpRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.PollableChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
-public class BacnetOutboundChannelAdapterTests {
+public class SnmpOutboundGatewayTests {
 
 	@BeforeClass
 	public static void beforeClass() throws Exception {
-		BacnetSlave.startup(37804);
+		SnmpAgent.startup(10173);
 	}
 
 	@AfterClass
 	public static void afterClass() throws Exception{
-		BacnetSlave.shutdown();
+		SnmpAgent.shutdown(10173);
 	}
 
 	protected Log logger = LogFactory.getLog(getClass());
-	
-	@Autowired @Qualifier("bacnetRequest")
-	private MessageChannel bacnetRequest;
+
+	@Autowired @Qualifier("snmpRequest")
+	private MessageChannel snmpRequest;
+
+	@Autowired @Qualifier("snmpResponse")
+	private PollableChannel snmpResponse;
 
     @Test
 	public void testRunning() throws Exception {
 
-    	BacnetRequest payload = new BacnetRequest("127.0.0.1:37804", 37804);
-		bacnetRequest.send(MessageBuilder.withPayload(payload).build());
-		Thread.sleep(3000);
+    	SnmpRequest payload = new SnmpRequest("127.0.0.1", 10173, "1.3.6");
+    	snmpRequest.send(MessageBuilder.withPayload(payload).build());
+
+		Object receive = snmpResponse.receive(10000);
+		Assert.assertNotNull(receive);
+		logger.debug(receive);
 	}
 }
